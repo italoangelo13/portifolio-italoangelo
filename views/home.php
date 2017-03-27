c9ca400a275a1e24c80f0896cbc67d296f2f0941
Italo Angelo <italoangelo.ti@gmail.com>
2017-03-27 09:22:58 -0300

    dados pessoais

---
+++ views/home.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Portifólio - Italo Angelo</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-primary">
+              <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <span class="navbar-brand" >Menu</span>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#pessoais">Dados Pessoais <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#formacao">Formação Academica</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="#habilidade">Habilidades</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="#experiencias">Experiencias</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="#contato">contato</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+        </div>
+
+
+
+        <header class="row scrollSuave" id="pessoais" name="pessoais">
+            <div class="jumbotron jumbotron-fluid col-lg-12" style="background: -webkit-gradient(linear, left top, left bottom, from(#0ff), to(#09f)) repeat-x;">
+                <div class="container">
+                    <div class="row">
+                        <h1 class="col-lg-12 display-4"><center>Dados Pessoais</center></h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <center>
+                            <img class="img-thumbnail rounded-circle " src="../assets/img/foto.jpg" alt="Italo Angelo" width="100%">
+                            </center>
+                        </div>
+                        <div class="col-lg-7" style="color: #000;">
+                            <center>
+                            <h1 class="display-3 "><strong>Italo Angelo</strong></h1>
+                            <h2 class="display-6 ">Desenvolvedor Web</h2>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <table class="">
+                                            <tr>
+                                                <td style="text-align: right;" colspan="" rowspan="" headers=""><h3 class="badge badge-primary">Nome</h3></td>
+                                                <td colspan="" rowspan="" headers=""><h6>Ítalo Angelo do Espirito Santo</h6></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td style="text-align: right;" colspan="" rowspan="" headers=""><h3 class="badge badge-primary">Data de Nascimento</h3></td>
+                                                <td colspan="" rowspan="" headers=""><h6>01 de Dezembro de 2017</h6></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td style="text-align: right;" colspan="" rowspan="" headers=""><h3 class="badge badge-primary">Endereço</h3></td>
+                                                <td colspan="" rowspan="" headers=""><h6>Rua Santo Antonio, 104, Santana, Sabará - MG</h6></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td style="text-align: right;" colspan="" rowspan="" headers=""><h3 class="badge badge-primary">Estado Civíl</h3></td>
+                                                <td colspan="" rowspan="" headers=""><h6>Solteiro</h6></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td style="text-align: right;" colspan="" rowspan="" headers=""><h3 class="badge badge-primary">Nacionalidade</h3></td>
+                                                <td colspan="" rowspan="" headers=""><h6>Brasileiro</h6></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td style="text-align: right;" colspan="" rowspan="" headers=""><h3 class="badge badge-primary">Email</h3></td>
+                                                <td colspan="" rowspan="" headers=""><h6>italoangelo.ti@gmail.com</h6></td>
+                                            </tr>
+
+                                            <tr> 
+                                                <td style="text-align: right;" colspan="" rowspan="" headers=""><h3 class="badge badge-primary">Telefone</h3></td>
+                                                <td colspan="" rowspan="" headers=""><h6>(31) 97507-0886 || (31) 3674-5894</h6></td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <a href="https://www.facebook.com/italo.belford" ><img src="../assets/img/face.png" alt="Facebook" width="8%" style="margin:10px; border-radius: 50px; "></a>
+                                        <a href="" ><img src="../assets/img/linkedin.png" alt="LinkedIn" width="8%" style="margin:10px; border-radius: 50px; "></a>
+                                        <a href="https://github.com/italoangelo13" ><img src="../assets/img/github.png" alt="Git Hub" width="8%" style="margin:10px; border-radius: 50px;"></a>
+                                    </div>
+                                </div>
+                            </div>
+                            </center>
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
+        </header>
+
+        <section class="row scrollSuave" id="formacao" name="formacao">
+            <div class="jumbotron jumbotron-fluid col-lg-12" style="background: -webkit-gradient(linear, left top, left bottom, from(#0ff), to(#09f)) repeat-x;">
+              <div class="container">
+                <div class="row">
+                    <h1 class="col-lg-12 display-4"><center>Formação Acadêmica</center></h1>
+                </div>
+              </div>
+            </div>
+        </section>
+
+        <footer class="row">
+            
+        </footer>
+    </div>
+
+
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+
+    <script>
+    var $doc = $('html, body');
+    $('a').click(function() {
+        $doc.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+    </script>
+
+</body>
+</html>
